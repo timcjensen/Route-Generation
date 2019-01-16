2a19b4c0f70ca7960b97fd79a9c907d35e8e038d
Tim Jensen <timjensen757@gmail.com>
2019-01-16 12:03:52 -0600

    init

---
+++ src/RouteGeneration.java
@@ -0,0 +1,28 @@
+/*
+    Route Generation program
+    CS-410 Software Engineering: Iyengar
+    Due: 1/30/2019
+    Elky Ratliff
+    Tim Jensen
+ */
+
+/*
+    Input:
+            Address.dat, a list of random addresses
+            Layout.dat, a list of adjacent streets
+    Output:
+            Route.dat, an organized list of addresses
+                        that is a suggested route
+ */
+
+import java.io.File;
+import java.io.FileReader;
+
+public class RouteGeneration {
+
+    String addressSource = "Address.dat";
+    String layoutSource = "Layout.dat";
+    public static void main(String[] args){
+
+    }
+}
