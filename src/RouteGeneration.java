d014596a238361be6cc2c22730f725e95a3af6c8
Tim Jensen <timjensen757@gmail.com>
2019-01-16 12:28:30 -0600

    additional info

--- src/RouteGeneration.java
+++ src/RouteGeneration.java
@@ -15,14 +15,51 @@
                         that is a suggested route
  */
 
+/*
+    TODO:
+        - Fill in test data (Address, Layout)
+        - Parsing in data
+        - Sorting addresses based on adjacency
+            - Grouped by city and zip code
+        - Writing result to file
+ */
+
 import java.io.File;
 import java.io.FileReader;
 
 public class RouteGeneration {
 
-    String addressSource = "Address.dat";
-    String layoutSource = "Layout.dat";
     public static void main(String[] args){
+        String addressSource = "Address.dat";
+        String layoutSource = "Layout.dat";
+
+        String[] addressA = parseAddress(addressSource);
+        String[] layoutA = parseLayout(layoutSource);
+
+        String output = determineRoute(addressA, layoutA);
+
+        writeResult(output);
+    }
+
+    //TODO: addresses can be parsed in a straightforward way
+    private String[] parseAddress(String source){
+        String[] result;
+        return result;
+    }
+
+    //TODO: layout is trickier - consider difference of cities, zips, vertical/horizontal
+    private String[] parseLayout(String source){
+        String[] result;
+        return result;
+    }
+
+    //TODO
+    private static String determineRoute(String[] addresses, String[] layout){
+        return "";
+    }
+
+    //TODO
+    private static void writeResult(String output){
 
     }
 }
