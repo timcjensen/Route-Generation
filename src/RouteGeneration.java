653c530700e0b6ed7af0158b1204dff7ed6f8104
ElkyLarenz <33266684+ElkyLarenz@users.noreply.github.com>
2019-02-03 17:23:58 -0600

    Update RouteGeneration.java

--- src/RouteGeneration.java
+++ src/RouteGeneration.java
@@ -26,6 +26,13 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+import javax.swing.*;
+import javax.swing.filechooser.FileNameExtensionFilter;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+
 public class RouteGeneration {
 
     private static BufferedReader reader;
@@ -57,6 +64,105 @@ public class RouteGeneration {
         }
 
         writeResult(routeList, outputFile);
+
+
+        JFrame frame = new JFrame("Route Generator");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(800,300);
+
+        JButton btn3 = new JButton("Show Address file");
+        JButton btn2 = new JButton("Show Layout file");
+        JButton btn1 = new JButton("Choose file");
+
+        btn3.setPreferredSize(new Dimension(200, 200));
+        btn2.setPreferredSize(new Dimension(200, 200));
+        btn1.setPreferredSize(new Dimension(200, 200));
+
+        frame.getContentPane().setLayout(new FlowLayout());
+        frame.getContentPane().add(btn3);
+        frame.getContentPane().add(btn2);
+        frame.getContentPane().add(btn1);
+
+        btn3.addActionListener( new ActionListener()
+        {
+            @Override
+            public void actionPerformed(ActionEvent e)
+            {
+
+                String fileName = "Address.dat";
+
+                String line = null;
+
+                try
+                {
+                    FileReader fileReader = new FileReader(fileName);
+                    BufferedReader b = new BufferedReader(fileReader);
+                    while((line = b.readLine()) != null)
+                    {
+                        System.out.println(line);
+                    }
+                    b.close();
+                }
+                catch(FileNotFoundException ex)
+                {
+                    System.out.println("Unable to open file '" + fileName + "'");
+                }
+                catch(IOException ex)
+                {
+                    System.out.println("Error reading file '" + fileName + "'");
+                }
+            }
+        });
+
+        btn2.addActionListener( new ActionListener()
+        {
+            @Override
+            public void actionPerformed(ActionEvent e)
+            {
+                String fileName = "Layout.dat";
+                String line = null;
+
+                try
+                {
+                    FileReader fileReader = new FileReader(fileName);
+                    BufferedReader b = new BufferedReader(fileReader);
+                    while((line = b.readLine()) != null)
+                    {
+                        System.out.println(line);
+                    }
+                    b.close();
+                }
+                catch(FileNotFoundException ex)
+                {
+                    System.out.println("Unable to open file '" + fileName + "'");
+                }
+                catch(IOException ex)
+                {
+                    System.out.println("Error reading file '" + fileName + "'");
+                }
+            }
+        });
+
+        btn1.addActionListener( new ActionListener()
+        {
+            @Override
+            public void actionPerformed(ActionEvent e)
+            {
+
+                JFileChooser chooser = new JFileChooser();
+                FileNameExtensionFilter filter = new FileNameExtensionFilter("text", "dat", "txt");
+                chooser.setFileFilter(filter);
+                int returnVal = chooser.showOpenDialog(null);
+                if(returnVal == JFileChooser.APPROVE_OPTION) {
+                    System.out.println("You chose to open this file: " + chooser.getSelectedFile().getName());
+                    chooser.getSelectedFile();
+
+                }
+            }
+        });
+
+        frame.setVisible(true);
+
     }
 
     //addresses can be parsed in a straightforward way
